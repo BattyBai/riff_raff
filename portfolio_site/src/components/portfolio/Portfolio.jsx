e79cbfc84e9a577e8aea318b9d52043c36850e21
Isaac Pure <isaacpure@gmail.com>
2022-10-10 17:10:09 -0500

    wroking on slider/carousel

--- portfolio_site/src/components/portfolio/Portfolio.jsx
+++ portfolio_site/src/components/portfolio/Portfolio.jsx
@@ -1,63 +1,51 @@
 import React from 'react'
-import { useState, useEffect } from 'react';
-import PortfolioList from '../portfolioList/PortfolioList';
 import './portfolio.scss'
 
 export default function Portfolio() {
-  const [selected, setSelected] = useState('featured')
-  const list = [
-    {
-      id: 'featured',
-      title: 'Featured',
-    },
-    {
-      id: 'web',
-      title: 'Web'
-    },
-    {
-      id: 'mobile',
-      title: 'Mobile'
-    },
-    {
-      id: 'design',
-      title: 'Design'
-    },
-    {
-      id: 'content',
-      title: 'Content'
-    }
-  ];
 
+  const data = [
+    {
+      id: "1",
+      title: "KneadIt",
+      desc: "Recipe baking app using JavaScript, Express.js and MongoDB",
+      img: "assets/kneadIt.jpeg"
+    },
+    {
+      id: '2',
+      title: 'The App.Post',
+      desc: 'A satirical news posting site using JavaScript, Django, and PostgreSQL',
+      img: 'assets/news-project-front.jpeg'
+    },
+    {
+      id: '3',
+      title: 'Budget Buddy',
+      desc: 'Account budgeting app allowing users to input transactions to keep track of their balance. Built using Ruby, Ruby on Rail and PostgreSQL',
+      img: 'assets/budgtbuddy.jpeg'
+    }
+  ]
   return (
     <div className='portfolio' id="portfolio">
-      <h1>Projects</h1>
-      <ul>
-        {list.map((item)=>(
-          <PortfolioList 
-          title={item.title} 
-          active={selected=== item.id} 
-          setSelected={setSelected}
-          id={item.id}
-          />
-        ))}
-      </ul>
-      <div className="container">
-        <div className="item">
-          <img src="assets/kneadIt.jpeg" alt="" />
-          <h3>Baking/Recipe App</h3>
-          <h4>JavaScript, Express.js, MongoDB</h4>
-        </div>
-        <div className="item">
-          <img src="assets/news-project-front.jpeg" alt="" />
-          <h3>Baking/Recipe App</h3>
-          <h4>JavaScript, Express.js, MongoDB</h4>
-        </div>
-        <div className="item">
-          <img src="assets/budgtbuddy.jpeg" alt="" />
-          <h3>Baking/Recipe App</h3>
-          <h4>JavaScript, Express.js, MongoDB</h4>
-        </div>
+      <div className="slider">
+        {<div className="container">
+          <div className="item">
+            <div className="left">
+              <div className="leftContainer">
+                {/* <div className="imageContainer">
+                  <img src="" alt="" />
+                </div> */}
+                <h2>Title</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda placeat cumque rem iusto cum veniam eveniet. Eligendi veritatis dolorem.</p>
+                <span>Projects</span>
+              </div>
+            </div>
+            <div className="right">
+              <img src="assets/news-project-front.jpeg" alt="" />
+            </div>
+          </div>
+        </div>}
       </div>
+      <img src="assets/004-down-arrow.png" className='arrow left' alt="" />
+      <img src="assets/004-down-arrow.png" className='arrow right' alt="" />
     </div>
   )
 }
